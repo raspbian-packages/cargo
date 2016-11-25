3085dd5ff6a9994b39670d7b3b3397dcca2b619f
Luca Bruno <lucab@debian.org>
2016-11-25 23:30:03 +0000

    Import cargo_0.15.0~dev.orig.tar.gz
    [dgit import orig cargo_0.15.0~dev.orig.tar.gz]

---
+++ src/bin/cargo.rs
@@ -0,0 +1,375 @@
+extern crate cargo;
+extern crate url;
+extern crate env_logger;
+extern crate git2_curl;
+extern crate rustc_serialize;
+extern crate toml;
+#[macro_use] extern crate log;
+
+use std::collections::BTreeSet;
+use std::collections::HashMap;
+use std::env;
+use std::fs;
+use std::path::{Path,PathBuf};
+
+use cargo::core::shell::Verbosity;
+use cargo::execute_main_without_stdin;
+use cargo::util::{self, CliResult, lev_distance, Config, human, CargoResult};
+use cargo::util::CliError;
+
+#[derive(RustcDecodable)]
+pub struct Flags {
+    flag_list: bool,
+    flag_version: bool,
+    flag_verbose: u32,
+    flag_quiet: Option<bool>,
+    flag_color: Option<String>,
+    flag_explain: Option<String>,
+    arg_command: String,
+    arg_args: Vec<String>,
+    flag_locked: bool,
+    flag_frozen: bool,
+}
+
+const USAGE: &'static str = "
+Rust's package manager
+
+Usage:
+    cargo <command> [<args>...]
+    cargo [options]
+
+Options:
+    -h, --help          Display this message
+    -V, --version       Print version info and exit
+    --list              List installed commands
+    --explain CODE      Run `rustc --explain CODE`
+    -v, --verbose ...   Use verbose output
+    -q, --quiet         No output printed to stdout
+    --color WHEN        Coloring: auto, always, never
+    --frozen            Require Cargo.lock and cache are up to date
+    --locked            Require Cargo.lock is up to date
+
+Some common cargo commands are (see all commands with --list):
+    build       Compile the current project
+    clean       Remove the target directory
+    doc         Build this project's and its dependencies' documentation
+    new         Create a new cargo project
+    init        Create a new cargo project in an existing directory
+    run         Build and execute src/main.rs
+    test        Run the tests
+    bench       Run the benchmarks
+    update      Update dependencies listed in Cargo.lock
+    search      Search registry for crates
+    publish     Package and upload this project to the registry
+    install     Install a Rust binary
+
+See 'cargo help <command>' for more information on a specific command.
+";
+
+fn main() {
+    env_logger::init().unwrap();
+    execute_main_without_stdin(execute, true, USAGE)
+}
+
+macro_rules! each_subcommand{
+    ($mac:ident) => {
+        $mac!(bench);
+        $mac!(build);
+        $mac!(clean);
+        $mac!(doc);
+        $mac!(fetch);
+        $mac!(generate_lockfile);
+        $mac!(git_checkout);
+        $mac!(help);
+        $mac!(init);
+        $mac!(install);
+        $mac!(locate_project);
+        $mac!(login);
+        $mac!(metadata);
+        $mac!(new);
+        $mac!(owner);
+        $mac!(package);
+        $mac!(pkgid);
+        $mac!(publish);
+        $mac!(read_manifest);
+        $mac!(run);
+        $mac!(rustc);
+        $mac!(rustdoc);
+        $mac!(search);
+        $mac!(test);
+        $mac!(uninstall);
+        $mac!(update);
+        $mac!(verify_project);
+        $mac!(version);
+        $mac!(yank);
+    }
+}
+
+macro_rules! declare_mod {
+    ($name:ident) => ( pub mod $name; )
+}
+each_subcommand!(declare_mod);
+
+/**
+  The top-level `cargo` command handles configuration and project location
+  because they are fundamental (and intertwined). Other commands can rely
+  on this top-level information.
+*/
+fn execute(flags: Flags, config: &Config) -> CliResult<Option<()>> {
+    config.configure(flags.flag_verbose,
+                     flags.flag_quiet,
+                     &flags.flag_color,
+                     flags.flag_frozen,
+                     flags.flag_locked)?;
+
+    init_git_transports(config);
+    let _token = cargo::util::job::setup();
+
+    if flags.flag_version {
+        println!("{}", cargo::version());
+        return Ok(None)
+    }
+
+    if flags.flag_list {
+        println!("Installed Commands:");
+        for command in list_commands(config) {
+            println!("    {}", command);
+        };
+        return Ok(None)
+    }
+
+    if let Some(ref code) = flags.flag_explain {
+        let mut procss = config.rustc()?.process();
+        procss.arg("--explain").arg(code).exec().map_err(human)?;
+        return Ok(None)
+    }
+
+    let args = match &flags.arg_command[..] {
+        // For the commands `cargo` and `cargo help`, re-execute ourselves as
+        // `cargo -h` so we can go through the normal process of printing the
+        // help message.
+        "" | "help" if flags.arg_args.is_empty() => {
+            config.shell().set_verbosity(Verbosity::Verbose);
+            let args = &["cargo".to_string(), "-h".to_string()];
+            let r = cargo::call_main_without_stdin(execute, config, USAGE, args,
+                                                   false);
+            cargo::process_executed(r, &mut config.shell());
+            return Ok(None)
+        }
+
+        // For `cargo help -h` and `cargo help --help`, print out the help
+        // message for `cargo help`
+        "help" if flags.arg_args[0] == "-h" ||
+                  flags.arg_args[0] == "--help" => {
+            vec!["cargo".to_string(), "help".to_string(), "-h".to_string()]
+        }
+
+        // For `cargo help foo`, print out the usage message for the specified
+        // subcommand by executing the command with the `-h` flag.
+        "help" => vec!["cargo".to_string(), flags.arg_args[0].clone(),
+                       "-h".to_string()],
+
+        // For all other invocations, we're of the form `cargo foo args...`. We
+        // use the exact environment arguments to preserve tokens like `--` for
+        // example.
+        _ => {
+            let mut default_alias = HashMap::new();
+            default_alias.insert("b", "build".to_string());
+            default_alias.insert("t", "test".to_string());
+            default_alias.insert("r", "run".to_string());
+            let mut args: Vec<String> = env::args().collect();
+            if let Some(new_command) = default_alias.get(&args[1][..]){
+                args[1] = new_command.clone();
+            }
+            args
+        }
+    };
+
+    if try_execute(&config, &args) {
+        return Ok(None)
+    }
+
+    let alias_list = aliased_command(&config, &args[1])?;
+    let args = match alias_list {
+        Some(alias_command) => {
+            let chain = args.iter().take(1)
+                .chain(alias_command.iter())
+                .chain(args.iter().skip(2))
+                .map(|s| s.to_string())
+                .collect::<Vec<_>>();
+            if try_execute(&config, &chain) {
+                return Ok(None)
+            } else {
+                chain
+            }
+        }
+        None => args,
+    };
+    execute_subcommand(config, &args[1], &args)?;
+    Ok(None)
+}
+
+fn try_execute(config: &Config, args: &[String]) -> bool {
+    macro_rules! cmd {
+        ($name:ident) => (if args[1] == stringify!($name).replace("_", "-") {
+            config.shell().set_verbosity(Verbosity::Verbose);
+            let r = cargo::call_main_without_stdin($name::execute, config,
+                                                   $name::USAGE,
+                                                   &args,
+                                                   false);
+            cargo::process_executed(r, &mut config.shell());
+            return true
+        })
+    }
+    each_subcommand!(cmd);
+
+    return false
+}
+
+fn aliased_command(config: &Config, command: &String) -> CargoResult<Option<Vec<String>>> {
+    let alias_name = format!("alias.{}", command);
+    let mut result = Ok(None);
+    match config.get_string(&alias_name) {
+        Ok(value) => {
+            if let Some(record) = value {
+                let alias_commands = record.val.split_whitespace()
+                                               .map(|s| s.to_string())
+                                               .collect();
+                result = Ok(Some(alias_commands));
+            }
+        },
+        Err(_) => {
+            let value = config.get_list(&alias_name)?;
+            if let Some(record) = value {
+                let alias_commands: Vec<String> = record.val.iter()
+                                .map(|s| s.0.to_string()).collect();
+                result = Ok(Some(alias_commands));
+            }
+        }
+    }
+    result
+}
+
+fn find_closest(config: &Config, cmd: &str) -> Option<String> {
+    let cmds = list_commands(config);
+    // Only consider candidates with a lev_distance of 3 or less so we don't
+    // suggest out-of-the-blue options.
+    let mut filtered = cmds.iter().map(|c| (lev_distance(&c, cmd), c))
+                                  .filter(|&(d, _)| d < 4)
+                                  .collect::<Vec<_>>();
+    filtered.sort_by(|a, b| a.0.cmp(&b.0));
+    filtered.get(0).map(|slot| slot.1.clone())
+}
+
+fn execute_subcommand(config: &Config,
+                      cmd: &str,
+                      args: &[String]) -> CliResult<()> {
+    let command_exe = format!("cargo-{}{}", cmd, env::consts::EXE_SUFFIX);
+    let path = search_directories(config)
+                    .iter()
+                    .map(|dir| dir.join(&command_exe))
+                    .find(|file| is_executable(file));
+    let command = match path {
+        Some(command) => command,
+        None => {
+            return Err(human(match find_closest(config, cmd) {
+                Some(closest) => format!("no such subcommand: `{}`\n\n\t\
+                                          Did you mean `{}`?\n", cmd, closest),
+                None => format!("no such subcommand: `{}`", cmd)
+            }).into())
+        }
+    };
+    let err = match util::process(&command).args(&args[1..]).exec() {
+        Ok(()) => return Ok(()),
+        Err(e) => e,
+    };
+
+    if let Some(code) = err.exit.as_ref().and_then(|c| c.code()) {
+        Err(CliError::code(code))
+    } else {
+        Err(CliError::new(Box::new(err), 101))
+    }
+}
+
+/// List all runnable commands. find_command should always succeed
+/// if given one of returned command.
+fn list_commands(config: &Config) -> BTreeSet<String> {
+    let prefix = "cargo-";
+    let suffix = env::consts::EXE_SUFFIX;
+    let mut commands = BTreeSet::new();
+    for dir in search_directories(config) {
+        let entries = match fs::read_dir(dir) {
+            Ok(entries) => entries,
+            _ => continue
+        };
+        for entry in entries.filter_map(|e| e.ok()) {
+            let path = entry.path();
+            let filename = match path.file_name().and_then(|s| s.to_str()) {
+                Some(filename) => filename,
+                _ => continue
+            };
+            if !filename.starts_with(prefix) || !filename.ends_with(suffix) {
+                continue
+            }
+            if is_executable(entry.path()) {
+                let end = filename.len() - suffix.len();
+                commands.insert(filename[prefix.len()..end].to_string());
+            }
+        }
+    }
+
+    macro_rules! add_cmd {
+        ($cmd:ident) => ({ commands.insert(stringify!($cmd).replace("_", "-")); })
+    }
+    each_subcommand!(add_cmd);
+    commands
+}
+
+#[cfg(unix)]
+fn is_executable<P: AsRef<Path>>(path: P) -> bool {
+    use std::os::unix::prelude::*;
+    fs::metadata(path).map(|metadata| {
+        metadata.is_file() && metadata.permissions().mode() & 0o111 != 0
+    }).unwrap_or(false)
+}
+#[cfg(windows)]
+fn is_executable<P: AsRef<Path>>(path: P) -> bool {
+    fs::metadata(path).map(|metadata| metadata.is_file()).unwrap_or(false)
+}
+
+fn search_directories(config: &Config) -> Vec<PathBuf> {
+    let mut dirs = vec![config.home().clone().into_path_unlocked().join("bin")];
+    if let Some(val) = env::var_os("PATH") {
+        dirs.extend(env::split_paths(&val));
+    }
+    dirs
+}
+
+fn init_git_transports(config: &Config) {
+    // Only use a custom transport if a proxy is configured, right now libgit2
+    // doesn't support proxies and we have to use a custom transport in this
+    // case. The custom transport, however, is not as well battle-tested.
+    match cargo::ops::http_proxy_exists(config) {
+        Ok(true) => {}
+        _ => return
+    }
+
+    let handle = match cargo::ops::http_handle(config) {
+        Ok(handle) => handle,
+        Err(..) => return,
+    };
+
+    // The unsafety of the registration function derives from two aspects:
+    //
+    // 1. This call must be synchronized with all other registration calls as
+    //    well as construction of new transports.
+    // 2. The argument is leaked.
+    //
+    // We're clear on point (1) because this is only called at the start of this
+    // binary (we know what the state of the world looks like) and we're mostly
+    // clear on point (2) because we'd only free it after everything is done
+    // anyway
+    unsafe {
+        git2_curl::register(handle);
+    }
+}
---
+++ src/cargo/ops/resolve.rs
@@ -0,0 +1,171 @@
+use std::collections::HashSet;
+
+use core::{PackageId, PackageIdSpec, SourceId, Workspace};
+use core::registry::PackageRegistry;
+use core::resolver::{self, Resolve, Method};
+use ops;
+use util::CargoResult;
+
+/// Resolve all dependencies for the specified `package` using the previous
+/// lockfile as a guide if present.
+///
+/// This function will also write the result of resolution as a new
+/// lockfile.
+pub fn resolve_ws(registry: &mut PackageRegistry, ws: &Workspace)
+                   -> CargoResult<Resolve> {
+    let prev = ops::load_pkg_lockfile(ws)?;
+    let resolve = resolve_with_previous(registry, ws,
+                                        Method::Everything,
+                                        prev.as_ref(), None, &[])?;
+
+    // Avoid writing a lockfile if we are `cargo install`ing a non local package.
+    if ws.current_opt().map(|pkg| pkg.package_id().source_id().is_path()).unwrap_or(true) {
+        ops::write_pkg_lockfile(ws, &resolve)?;
+    }
+    Ok(resolve)
+}
+
+/// Resolve all dependencies for a package using an optional previous instance
+/// of resolve to guide the resolution process.
+///
+/// This also takes an optional hash set, `to_avoid`, which is a list of package
+/// ids that should be avoided when consulting the previous instance of resolve
+/// (often used in pairings with updates).
+///
+/// The previous resolve normally comes from a lockfile. This function does not
+/// read or write lockfiles from the filesystem.
+pub fn resolve_with_previous<'a>(registry: &mut PackageRegistry,
+                                 ws: &Workspace,
+                                 method: Method,
+                                 previous: Option<&'a Resolve>,
+                                 to_avoid: Option<&HashSet<&'a PackageId>>,
+                                 specs: &[PackageIdSpec])
+                                 -> CargoResult<Resolve> {
+    // Here we place an artificial limitation that all non-registry sources
+    // cannot be locked at more than one revision. This means that if a git
+    // repository provides more than one package, they must all be updated in
+    // step when any of them are updated.
+    //
+    // TODO: This seems like a hokey reason to single out the registry as being
+    //       different
+    let mut to_avoid_sources = HashSet::new();
+    if let Some(to_avoid) = to_avoid {
+        to_avoid_sources.extend(to_avoid.iter()
+                                        .map(|p| p.source_id())
+                                        .filter(|s| !s.is_registry()));
+    }
+
+    // In the case where a previous instance of resolve is available, we
+    // want to lock as many packages as possible to the previous version
+    // without disturbing the graph structure. To this end we perform
+    // two actions here:
+    //
+    // 1. We inform the package registry of all locked packages. This
+    //    involves informing it of both the locked package's id as well
+    //    as the versions of all locked dependencies. The registry will
+    //    then takes this information into account when it is queried.
+    //
+    // 2. The specified package's summary will have its dependencies
+    //    modified to their precise variants. This will instruct the
+    //    first step of the resolution process to not query for ranges
+    //    but rather for precise dependency versions.
+    //
+    //    This process must handle altered dependencies, however, as
+    //    it's possible for a manifest to change over time to have
+    //    dependencies added, removed, or modified to different version
+    //    ranges. To deal with this, we only actually lock a dependency
+    //    to the previously resolved version if the dependency listed
+    //    still matches the locked version.
+    if let Some(r) = previous {
+        trace!("previous: {:?}", r);
+        for node in r.iter().filter(|p| keep(p, to_avoid, &to_avoid_sources)) {
+            let deps = r.deps_not_replaced(node)
+                        .filter(|p| keep(p, to_avoid, &to_avoid_sources))
+                        .cloned().collect();
+            registry.register_lock(node.clone(), deps);
+        }
+    }
+
+    let mut summaries = Vec::new();
+    for member in ws.members() {
+        registry.add_sources(&[member.package_id().source_id().clone()])?;
+        let method_to_resolve = match method {
+            // When everything for a workspace we want to be sure to resolve all
+            // members in the workspace, so propagate the `Method::Everything`.
+            Method::Everything => Method::Everything,
+
+            // If we're not resolving everything though then the workspace is
+            // already resolved and now we're drilling down from that to the
+            // exact crate graph we're going to build. Here we don't necessarily
+            // want to keep around all workspace crates as they may not all be
+            // built/tested.
+            //
+            // Additionally, the `method` specified represents command line
+            // flags, which really only matters for the current package
+            // (determined by the cwd). If other packages are specified (via
+            // `-p`) then the command line flags like features don't apply to
+            // them.
+            //
+            // As a result, if this `member` is the current member of the
+            // workspace, then we use `method` specified. Otherwise we use a
+            // base method with no features specified but using default features
+            // for any other packages specified with `-p`.
+            Method::Required { dev_deps, .. } => {
+                assert!(previous.is_some());
+                let base = Method::Required {
+                    dev_deps: dev_deps,
+                    features: &[],
+                    uses_default_features: true,
+                };
+                let member_id = member.package_id();
+                match ws.current_opt() {
+                    Some(current) if member_id == current.package_id() => method,
+                    _ => {
+                        if specs.iter().any(|spec| spec.matches(member_id)) {
+                            base
+                        } else {
+                            continue
+                        }
+                    }
+                }
+            }
+        };
+
+        let summary = registry.lock(member.summary().clone());
+        summaries.push((summary, method_to_resolve));
+    }
+
+    let root_replace = ws.root_replace();
+
+    let replace = match previous {
+        Some(r) => {
+            root_replace.iter().map(|&(ref spec, ref dep)| {
+                for (key, val) in r.replacements().iter() {
+                    if spec.matches(key) &&
+                       dep.matches_id(val) &&
+                       keep(&val, to_avoid, &to_avoid_sources) {
+                        return (spec.clone(), dep.clone().lock_to(val))
+                    }
+                }
+                (spec.clone(), dep.clone())
+            }).collect::<Vec<_>>()
+        }
+        None => root_replace.to_vec(),
+    };
+
+    let mut resolved = resolver::resolve(&summaries, &replace, registry)?;
+    if let Some(previous) = previous {
+        resolved.merge_from(previous)?;
+    }
+    return Ok(resolved);
+
+    fn keep<'a>(p: &&'a PackageId,
+                to_avoid_packages: Option<&HashSet<&'a PackageId>>,
+                to_avoid_sources: &HashSet<&'a SourceId>)
+                -> bool {
+        !to_avoid_sources.contains(&p.source_id()) && match to_avoid_packages {
+            Some(set) => !set.contains(p),
+            None => true,
+        }
+    }
+}
