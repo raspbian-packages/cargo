7e7db8e34d098eb27ae148b96b5556a7d018aee5
Rust Maintainers <pkg-rust-maintainers@alioth-lists.debian.net>
2018-11-09 10:56:54 +0000

    _sparc64_disable_incremental_build
    Gbp-Pq: Name 2007_sparc64_disable_incremental_build.patch

--- tests/testsuite/build.rs
+++ tests/testsuite/build.rs
@@ -38,6 +38,7 @@ fn cargo_fail_with_no_stderr() {
 
 /// Check that the `CARGO_INCREMENTAL` environment variable results in
 /// `rustc` getting `-Zincremental` passed to it.
+#[cfg(not(target_arch = "sparc64"))]
 #[test]
 fn cargo_compile_incremental() {
     let p = project()
@@ -58,6 +59,7 @@ fn cargo_compile_incremental() {
         ).run();
 }
 
+#[cfg(not(target_arch = "sparc64"))]
 #[test]
 fn incremental_profile() {
     let p = project()
@@ -99,6 +101,7 @@ fn incremental_profile() {
         .run();
 }
 
+#[cfg(not(target_arch = "sparc64"))]
 #[test]
 fn incremental_config() {
     let p = project()
