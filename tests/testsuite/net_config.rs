ee075749434a56d88f754b461b620bc5f838bb8f
Rust Maintainers <pkg-rust-maintainers@alioth-lists.debian.net>
2018-12-11 15:27:21 +0000

    _disable-net-tests
    Gbp-Pq: Name 2002_disable-net-tests.patch

--- tests/testsuite/net_config.rs
+++ tests/testsuite/net_config.rs
@@ -1,6 +1,6 @@
 use support::project;
 
-#[test]
+#[allow(dead_code)]
 fn net_retry_loads_from_config() {
     let p = project()
         .file(
@@ -33,7 +33,7 @@ fn net_retry_loads_from_config() {
         ).run();
 }
 
-#[test]
+#[allow(dead_code)]
 fn net_retry_git_outputs_warning() {
     let p = project()
         .file(
