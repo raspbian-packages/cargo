255c0ffd98c8b05b3214a0fd82f097addd9df8c2
Rust Maintainers <pkg-rust-maintainers@alioth-lists.debian.net>
2020-01-05 13:31:46 +0000

    _disable_fetch_cross_tests
    Gbp-Pq: Name 2005_disable_fetch_cross_tests.patch

--- tests/testsuite/fetch.rs
+++ tests/testsuite/fetch.rs
@@ -12,8 +12,8 @@ fn no_deps() {
     p.cargo("fetch").with_stdout("").run();
 }
 
-#[cargo_test]
-fn fetch_all_platform_dependencies_when_no_target_is_given() {
+#[allow(dead_code)]
+fn _fetch_all_platform_dependencies_when_no_target_is_given() {
     if cross_compile::disabled() {
         return;
     }
@@ -59,8 +59,8 @@ fn fetch_all_platform_dependencies_when_no_target_is_given() {
         .run();
 }
 
-#[cargo_test]
-fn fetch_platform_specific_dependencies() {
+#[allow(dead_code)]
+fn _fetch_platform_specific_dependencies() {
     if cross_compile::disabled() {
         return;
     }
